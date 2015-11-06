bfcf91e4766ee824969a746ddc441699a545559a
shubal <zxfrtas@naver.com>
2015-11-06 14:27:25 +0900

    xxx

--- src/com/hybrid/hybridweb/HybridWebActivity.java
+++ src/com/hybrid/hybridweb/HybridWebActivity.java
@@ -5,20 +5,34 @@ import android.os.Bundle;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.view.View.OnClickListener;
 import android.webkit.JavascriptInterface;
 import android.webkit.WebChromeClient;
 import android.webkit.WebSettings;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
+import android.widget.Button;
 import android.widget.Toast;
 
-public class HybridWebActivity extends Activity {
+public class HybridWebActivity extends Activity implements OnClickListener{
+	
+	Button btnWeb80;
+	Button btnWeb8080;
+	Button star;
 	
 	WebView myweb;
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_hybrid_web);
+		
+		btnWeb80=(Button) findViewById(R.id.btnWeb80);
+		btnWeb8080=(Button) findViewById(R.id.btnWeb8080);
+		star=(Button) findViewById(R.id.star);
+		btnWeb80.setOnClickListener(this);
+		btnWeb8080.setOnClickListener(this);
+		star.setOnClickListener(this);
 		
 		myweb = (WebView) findViewById(R.id.myweb);
 		
@@ -29,8 +43,9 @@ public class HybridWebActivity extends Activity {
 		myweb.setWebViewClient(new MyWebViewClient());		//a href
 		myweb.setWebChromeClient(new WebChromeClient());	//alert()
 		
-//		myweb.loadUrl("http://192.168.10.9:8080/web/index.jsp");
-		myweb.loadUrl("http://192.168.10.9");
+//		myweb.loadUrl("http://192.168.10.9:8080/web/index.jsp");		
+//		myweb.loadUrl("http://192.168.10.9");
+		myweb.loadUrl("http://www.soen.kr");
 	}
 
 	class MyJavascriptInterface {
@@ -68,5 +83,22 @@ public class HybridWebActivity extends Activity {
 			return true;
 		}
 		return super.onOptionsItemSelected(item);
+	}
+
+	@Override
+	public void onClick(View v) {
+		switch (v.getId()){
+		case R.id.btnWeb80:
+			myweb.loadUrl("http://192.168.10.9");
+		break;
+		case R.id.btnWeb8080:
+			myweb.loadUrl("http://192.168.10.9:8080/web");
+		break;
+		case R.id.star:
+			myweb.loadUrl("https://www.youtube.com/watch?v=A5AmE_b68cg");
+		break;
+		default:
+		break;
+		}
 	}
 }
